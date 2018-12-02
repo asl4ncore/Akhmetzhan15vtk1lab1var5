21b4bd20b2b8d0e88b54fbec8b6f0285d6e2e510
asl4ncore <asl4ncore@gmail.com>
2018-12-03 03:55:06 +0600

    Добавьте файлы проекта.

---
+++ TestNinja.Unit.Tests/MathLabTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.Unit.Tests
+{
+    [TestClass]
+    public class MathLabTests
+    {
+        [TestMethod]
+        public void CanBeCancelledBy_MultiplicationGreaterThan10__ReturnsTrue()
+        {
+            #region Arrange
+            var comparison = new Comparison();
+            #endregion
+
+            #region Act
+            var result = comparison.MultiplicationGreaterThan10(2, 6);
+            #endregion
+
+            #region Assert
+            Assert.IsTrue(result);
+            #endregion
+        }
+
+        [TestMethod]
+        public void CanBeCancelledBy_MultiplicationGreaterThan10__ReturnsFalse()
+        {
+            #region Arrange
+            var comparison = new Comparison();
+            #endregion
+
+            #region Act
+            var result = comparison.MultiplicationGreaterThan10(2, 4);
+            #endregion
+
+            #region Assert
+            Assert.IsFalse(result);
+            #endregion
+        }
+
+        [TestMethod]
+        public void CanBeCancelledBy_MultiplicationGreaterThan10__ReturnsIsNotNull()
+        {
+            #region Arrange
+            var comparison = new Comparison();
+            #endregion
+
+            #region Act
+            var result = comparison.MultiplicationGreaterThan10(2, 5);
+            #endregion
+
+            #region Assert
+            Assert.IsNotNull(result);
+            #endregion
+        }
+    }
+}
---
+++ TestNinja.Unit.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestNinja.Unit.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestNinja.Unit.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("173a28c0-4c14-4301-bf22-287865068f17")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
